d9115ffbd375a4265bb5122123c6c4b9772d40e8
Ashley Grenon <ashley.grenon@gmail.com>
2019-10-12 22:12:50 -0500

    Updating the pixel counter to draw a legend image

--- pixel-color-count.py
+++ pixel-color-count.py
@@ -2,7 +2,7 @@
 import argparse
 
 try:
-    from PIL import Image
+    from PIL import Image, ImageDraw, ImageFont
 except ImportError:
     exit("This script requires the PIL module. Install with pip install Pillow")
 
@@ -18,9 +18,9 @@ def main():
     # parser.add_argument('-i', '--ignore-color', type=tuple, help='Skip counting pixels of this color')
 
     args = parser.parse_args()
+    color_count = {}
 
     with Image.open(args.image) as image:
-        color_count = {}
         width, height = image.size
         rgb_image = image.convert('RGB')
 
@@ -34,19 +34,39 @@ def main():
                 else:
                     color_count[rgb] = 1
                     
-        print('Pixel Count per Unique Color:')
-        print('-' * 30)
-        color_index = 1
-        for color, count in color_count.items():
-            # if color == args.ignore-color:
-            #    pass
+    # outputs pixel color count to console
+    print('Pixel Count per Unique Color:')
+    print('-' * 30)
+    color_index = 1
+    
+    margin = 10
+    font = ImageFont.truetype("arialbd.ttf", 20)
+    rect_width = 25
+    rect_outline_width = 2
+    legend_img = Image.new("RGBA", (200, len(color_count) * (rect_width + rect_outline_width + margin)))
+    draw = ImageDraw.Draw(legend_img)
+    for color, count in color_count.items():
+        # if color == args.ignore-color:
+        #    pass
 
-            try:
-                print('{}.) {}: {}'.format(color_index, webcolors.rgb_to_name(color), count))
-            except ValueError:                
-                print('{}.) {}: {}'.format(color_index, color, count))
-            color_index += 1
+        try:
+            color_name = webcolors.rgb_to_name(color)
+        except ValueError:                
+            color_name = color
+        
+        print('{}.) {}: {}'.format(color_index, color_name, count))        
 
+        # draw square for color legend
+        y0 = rect_width * (color_index - 1) + margin * color_index
+        y1 = rect_width * color_index + margin * color_index
+        draw.rectangle([(0, y0), (rect_width, y1)], fill=color, outline="black", width=2)
+
+        # draw text for legend
+        draw.text((rect_width + margin, y0), "{}: {}".format(color_name, count), font=font, fill="black")
+
+        color_index += 1
+
+    legend_img.save(r'legend.png', mode="w")
 
 if __name__ == '__main__':
     main()
