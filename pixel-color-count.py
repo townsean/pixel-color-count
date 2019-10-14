f28931ad2936c674f68be975988fee01a90006a4
Ashley Grenon <ashley.grenon@gmail.com>
2019-10-14 09:41:11 -0500

    Refactored the main function moving some functionality into separate functions

--- pixel-color-count.py
+++ pixel-color-count.py
@@ -1,4 +1,5 @@
 # pixel-color-count.py - Gets a sum of pixels per unique color
+
 import argparse
 from ast import literal_eval
 
@@ -13,17 +14,9 @@ except ImportError:
     # https://pypi.org/project/webcolors/
     exit("This script uses webcolors for displaying named colors. Install with pip install webcolors")
 
-def main():
-    parser = argparse.ArgumentParser(description='Calculates the sum of pixels per a color')
-    parser.add_argument('image', nargs='?', default='.', help='The image to sum the pixels per a color of')
-    parser.add_argument('-i', '--ignore-color', help='Skip counting pixels of this color')
-    parser.add_argument('-t', '--title', help='Title for the image legend')
-
-    args = parser.parse_args()
-    ignore_color = literal_eval(args.ignore_color) if args.ignore_color is not None else None
+def count_pixels(filename):
     color_count = {}
-
-    with Image.open(args.image) as image:
+    with Image.open(filename) as image:
         width, height = image.size
         rgb_image = image.convert('RGB')
 
@@ -36,29 +29,28 @@ def main():
                     color_count[rgb] += 1
                 else:
                     color_count[rgb] = 1
-                    
-    # outputs pixel color count to console
-    print('Pixel Count per Unique Color:')
-    print('-' * 30)
-    color_index = 1
-    
+
+    return color_count
+
+def create_legend_image(filename, colors, title, ignore_color):
     margin = 10
     rect_width = 25
     rect_outline_width = 2
     font_size = 20
     img_width = 250
-    img_height = len(color_count) * (rect_width + rect_outline_width + margin) + (0 if args.title is None else font_size)
+    img_height = len(colors) * (rect_width + rect_outline_width + margin) + (0 if title is None else font_size)
     legend_img = Image.new("RGBA", (img_width, img_height))
     draw = ImageDraw.Draw(legend_img)    
     font = ImageFont.truetype("arialbd.ttf", font_size)
-
+    
     # draw title for legend if applicable
     text_height = 0
-    if args.title is not None:
-        _, text_height = draw.textsize(args.title, font=font)
-        draw.text((0, 0), args.title, font=font, fill="black")
+    if title is not None:
+        _, text_height = draw.textsize(title, font=font)
+        draw.text((0, 0), title, font=font, fill="black")
 
-    for color, count in color_count.items():
+    color_index = 1
+    for color, count in colors.items():
         if color == ignore_color:
             continue
 
@@ -67,8 +59,6 @@ def main():
             color_name = webcolors.rgb_to_name(color)
         except ValueError:                
             color_name = color
-        
-        print('{}.) {}: {}'.format(color_index, color_name, count))        
 
         # draw square for color legend
         y0 = rect_width * (color_index - 1) + margin * color_index + text_height
@@ -78,9 +68,40 @@ def main():
         # draw color name next and pixel count for legend colors
         draw.text((rect_width + margin, y0), "{}: {}".format(color_name, count), font=font, fill="black")
 
-        color_index += 1    
+        color_index += 1
 
-    legend_img.save('legend.png', mode="w")
+    legend_img.save(filename, mode="w")
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculates the sum of pixels per a color')
+    parser.add_argument('image', nargs='?', default='.', help='The image to sum the pixels per a color of')
+    parser.add_argument('-i', '--ignore-color', help='Skip counting pixels of this color')
+    parser.add_argument('-t', '--title', help='Title for the image legend')
+    parser.add_argument('-l', '--legend-image', help='Generate an image with color swatches paired with the pixel count')
+
+    args = parser.parse_args()
+    ignore_color = literal_eval(args.ignore_color) if args.ignore_color is not None else None
+    color_count = count_pixels(args.image)                   
+
+    if args.legend_image is not None:
+        create_legend_image(args.legend_image, color_count, args.title, ignore_color)
+ 
+    # outputs pixel color count to console
+    print('Pixel Count per Unique Color:')
+    print('-' * 30)
+    color_index = 1
+
+    for color, count in color_count.items(): 
+        if color == ignore_color:
+            continue
+
+        try:
+            # convert RGB color to a human readable color if applicable
+            color_name = webcolors.rgb_to_name(color)
+        except ValueError:                
+            color_name = color
+
+        print('{}.) {}: {}'.format(color_index, color_name, count))
 
 if __name__ == '__main__':
     main()
